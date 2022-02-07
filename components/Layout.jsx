181b8ff9a221d6b594b85d9e2c6600f20d305dbc
ADevCalledMo <mozeswalker@outlook.com>
2022-02-07 09:07:09 +0000

    Universal Layout implementation

---
+++ components/Layout.jsx
@@ -0,0 +1,49 @@
+import React from "react";
+import Head from "next/head";
+import { useColorMode, Button, Flex, Box } from "@chakra-ui/react";
+// import Navbar from "./Navbar";
+// import Footer from "./Footer";
+
+const Layout = ({ children }) => {
+  const { colorMode } = useColorMode();
+  const bgColor = {
+    light: "white",
+    dark: "#171717",
+  };
+
+  const navHoverBg = {
+    light: "gray.600",
+    dark: "gray.300",
+  };
+  const color = {
+    light: "black",
+    black: "white",
+  };
+
+  return (
+    <>
+      <Head>
+        <title>Real Estate</title>
+      </Head>
+      <Box maxWidth="1280px" m="auto">
+        <header>
+          {/* <Navbar bg={bgColor[colorMode]} textColor={color[colorMode]} /> */}
+        </header>
+      </Box>
+      <Flex
+        as="main"
+        justifyContent="center"
+        flexDirection="column"
+        bg={bgColor[colorMode]}
+        color={color[colorMode]}
+      >
+        {children}
+      </Flex>
+      <footer>
+        {/* <Footer bg={bgColor[colorMode]} textColor={color[colorMode]} /> */}
+      </footer>
+    </>
+  );
+};
+
+export default Layout;
--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,12 @@ import Link from "next/link";
 import Image from "next/image";
 import { Box, Flex, Text, Button } from "@chakra-ui/react";
 
+import Layout from "../components/Layout";
+
 export default function Home() {
-  return <>Hello</>;
+  return (
+    <Layout>
+      <Text> Hello </Text>
+    </Layout>
+  );
 }
