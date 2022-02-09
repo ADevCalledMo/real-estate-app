eb70c9e545d2e62b23064d8781de50a15df4101a
ADevCalledMo <mozeswalker@outlook.com>
2022-02-09 07:13:33 +0000

    Banner Component Completed

---
+++ components/Banner.jsx
@@ -0,0 +1,38 @@
+import Link from "next/link";
+import Image from "next/image";
+import { Box, Text, Flex, Button } from "@chakra-ui/react";
+
+const Banner = ({
+  purpose,
+  title1,
+  title2,
+  desc1,
+  desc2,
+  linkName,
+  buttonText,
+  imageUrl,
+}) => (
+  <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="20">
+    <Image src={imageUrl} width={500} height={300} alt="banner" />
+    <Box p="5">
+      <Text fontSize="sm" fontWeight="medium">
+        {purpose}
+      </Text>
+      <Text fontSize="3xl" fontWeight="bold">
+        {title1}
+        <br />
+        {title2}
+      </Text>
+      <Text fontSize="lg" paddingTop="3" paddingBottom="3">
+        {desc1}
+        <br />
+        {desc2}
+      </Text>
+      <Button fontSize="xl">
+        <Link href={linkName}>{buttonText}</Link>
+      </Button>
+    </Box>
+  </Flex>
+);
+
+export default Banner;
--- pages/index.js
+++ pages/index.js
@@ -3,12 +3,24 @@ import Image from "next/image";
 import { Box, Flex, Text, Button } from "@chakra-ui/react";
 
 import Layout from "../components/Layout";
+import Banner from "../components/Banner";
 
 
 export default function Home() {
   return (
     <Layout>
-      <Text> Hello </Text>
+      <Box>
+      <Banner
+        purpose="Rent a Home"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      </Box>
     </Layout>
   );
 }
