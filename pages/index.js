04955f9f10f9d6cbd70ddcaf48f4fabdc358be8f
ADevCalledMo <mozeswalker@outlook.com>
2022-03-17 18:27:59 +0000

    Next Build Error

--- pages/index.js
+++ pages/index.js
@@ -9,38 +9,38 @@ import { baseUrl, fetchApi } from "../utils/fetchApi";
 
 export default function Home({ propertiesForSale, propertiesForRent }) {
   return (
-      <Box>
-        <Banner
-          purpose="Rent a Home"
-          title1="Rental Homes for"
-          title2="Everyone"
-          desc1="Explore Apartments, Villas, Homes"
-          desc2="and more"
-          buttonText="Explore Renting"
-          linkName="/search?purpose=for-rent"
-          imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
-        />
-        <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
-          {propertiesForRent.map((property) => (
-            <Property property={property} key={property.id} />
-          ))}
-        </Flex>
-        <Banner
-          purpose="BUY A HOME"
-          title1=" Find, Buy & Own Your"
-          title2="Dream Home"
-          desc1=" Explore from Apartments, land, builder floors,"
-          desc2=" villas and more"
-          buttonText="Explore Buying"
-          linkName="/search?purpose=for-sale"
-          imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
-        />
-        <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
-          {propertiesForSale.map((property) => (
-            <Property property={property} key={property.id} />
-          ))}
-        </Flex>
-      </Box>
+    <Box>
+      <Banner
+        purpose="Rent a Home"
+        title1="Rental Homes for"
+        title2="Everyone"
+        desc1="Explore Apartments, Villas, Homes"
+        desc2="and more"
+        buttonText="Explore Renting"
+        linkName="/search?purpose=for-rent"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/145426814/33973352624c48628e41f2ec460faba4"
+      />
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+        {propertiesForRent.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+      <Banner
+        purpose="BUY A HOME"
+        title1=" Find, Buy & Own Your"
+        title2="Dream Home"
+        desc1=" Explore from Apartments, land, builder floors,"
+        desc2=" villas and more"
+        buttonText="Explore Buying"
+        linkName="/search?purpose=for-sale"
+        imageUrl="https://bayut-production.s3.eu-central-1.amazonaws.com/image/110993385/6a070e8e1bae4f7d8c1429bc303d2008"
+      />
+      <Flex flexWrap="wrap" justifyContent="center" alignItems="center">
+        {propertiesForSale.map((property) => (
+          <Property property={property} key={property.id} />
+        ))}
+      </Flex>
+    </Box>
   );
 }
 
@@ -56,6 +56,7 @@ export async function getStaticProps() {
     props: {
       propertiesForSale: propertyForSale?.hits,
       propertiesForRent: propertyForRent?.hits,
+      fallback: true,
     },
   };
 }
